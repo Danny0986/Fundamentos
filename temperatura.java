9d71a7f1596f3b0ebd9e7f51f12a846d8915d523
sebastian-gonzalez593 <dvcobos1@utpl.edu.ec>
2025-02-04 21:27:46 -0500

    Actualizacion

--- temperatura.java
+++ temperatura.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class temperaturas {
+public class temperatura {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -110,4 +110,3 @@ public class temperaturas {
         }
     }
 }
-
